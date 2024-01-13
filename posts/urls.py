e70cbbe6b8d76086c4b917a400ad28161908cbff
Hwannni <cmhcms1115@naver.com>
2024-01-13 17:29:11 +0900

    2024.01.13

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import Main
+from .views import Main, UploadFeed
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', Main.as_view()),  # .as_view()를 사용하여 뷰를 지정
+    path('content/upload', UploadFeed.as_view())
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
