40ae48ff644e318e2457d1945703714381d6cfc3
Hwannni <cmhcms1115@naver.com>
2024-01-14 00:44:45 +0900

    2024.01.14

--- minstagram/urls.py
+++ minstagram/urls.py
@@ -6,5 +6,7 @@ from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('posts.urls'))
+    path("", include('posts.urls')),
+    #path("", include('content.urls')),
+    path("", include('user.urls')),
 ]
