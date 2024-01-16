2070717c07f203aaba7d9b2b7b7978447a082c2b
Hwannni <cmhcms1115@naver.com>
2024-01-16 20:37:42 +0900

    2024.01.16

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from content.models import Feed
+from user.models import User
 from rest_framework.response import Response
 import os
 from minstagram.settings import MEDIA_ROOT, MEDIA_URL
@@ -10,7 +11,17 @@ from uuid import uuid4
 class Main(APIView):
     def get(self, request):
         feed_list = Feed.objects.all().order_by('-id')
-        return render(request, 'minstagram/main.html', context=dict(feed_list=feed_list))
+        email = request.session['email']
+
+        if email is None:
+            return render(request, "common/login.html")
+        
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return render(request, "common/login.html")
+        
+        return render(request, 'minstagram/main.html', context=dict(feed_list=feed_list, user=user))
 
 class UploadFeed(APIView):
     def post(self, request):
